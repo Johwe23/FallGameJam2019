2bc03b0c5f00ee54d285b4ed3400f5ae085051d2
Jonatan Wästlund <jonte.w1@gmail.com>
2019-11-09 18:10:44 +0100

    Petal interaction

---
+++ FallGameJam2019/Assets/Scripts/Interaction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interaction : MonoBehaviour
+{
+    private GameObject[] interactables;
+    private GameObject closest;
+
+    public float pickupRange;
+    public GameObject Petal;
+    public GameObject PetalTruck;
+    public GameObject Scene;
+    private GameObject child = null;
+    private GameObject petal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")){
+            if (child == null)
+            {
+                interactables = GameObject.FindGameObjectsWithTag("Interactable");
+
+                closest = interactables[0];
+
+                foreach (GameObject interactable in interactables)
+                {
+                    if ((interactable.transform.position - transform.position).sqrMagnitude < (closest.transform.position - transform.position).sqrMagnitude)
+                    {
+                        closest = interactable;
+                    }
+                }
+                if ((closest.transform.position - transform.position).sqrMagnitude < pickupRange)
+                {
+                    Destroy(closest);
+                    child = Instantiate(PetalTruck, transform);
+                }
+            }
+            else {
+                Destroy(child);
+                petal = Instantiate(Petal);
+                petal.transform.position = transform.position;
+            }
+        }
+    }
+}
