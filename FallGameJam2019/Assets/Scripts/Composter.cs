63eba4180f999ead80be2825e6740386cb6c0011
Johan F <johan.furuhjelm@gmail.com>
2019-11-09 17:11:55 +0100

    Flowers and world
    Flower colors
    Flower timer and drops
    Compost timer and trigger
    World made beatiful

---
+++ FallGameJam2019/Assets/Scripts/Composter.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Composter : MonoBehaviour
+{
+    List<GameObject> petals = new List<GameObject>();
+
+    public double CountDownTime = 5;
+    private double timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer < 0){
+
+            makeCompost();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other){
+        petals.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other){
+        petals.Remove(other.gameObject);
+    }
+
+    private void makeCompost(){
+        
+    }
+}
---
+++ FallGameJam2019/Assets/Scripts/DroppedPetal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppedPetal : MonoBehaviour
+{
+
+    public double lifeTime = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime <= 0){
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
