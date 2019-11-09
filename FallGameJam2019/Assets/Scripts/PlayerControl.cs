c2d845afa0951ddd6befcc1c5b8bffdf85dd4890
Benjamin Sundvall <benjamin@urgott.se>
2019-11-09 13:22:00 +0100

    added movement

---
+++ FallGameJam2019/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //CharacterController characterController;
+
+    public CharacterController cont;
+
+    public float speed = 6.0f;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        cont.SimpleMove(new Vector3(speed * Input.GetAxis("Horizontal"), 0, speed * Input.GetAxis("Vertical")));
+        //print(Input.GetAxis("Horizontal"));
+        Vector2 facing = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).Angle; 
+        transform.eulerAngles = (0f, facing, 0f);
+    }
+}
