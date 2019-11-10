e6833d0777248b6a7bfb78409dd8a1f33d647671
Jonatan Wästlund <jonte.w1@gmail.com>
2019-11-10 14:24:50 +0100

    Fixed petals decaying

--- FallGameJam2019/Assets/Scripts/FlowerPetal.cs
+++ FallGameJam2019/Assets/Scripts/FlowerPetal.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class FlowerPetal : MonoBehaviour
 {
-
+    public float deathTime;
+    private float currentTime;
+    public float shrinkTime;
+    private float passedTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gameObject.tag == "Petal") {
+            Destroy(gameObject, deathTime);
+            currentTime = 0f;
+            passedTime = shrinkTime;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.localScale = 
+        if (gameObject.tag == "Petal")
+        {
+            if (currentTime > deathTime-shrinkTime)
+            {
+                passedTime -= Time.deltaTime;
+                transform.localScale = new Vector3(1, 1, 1) * passedTime / shrinkTime;
+            }
+            currentTime += Time.deltaTime;
+        }
     }
 }
